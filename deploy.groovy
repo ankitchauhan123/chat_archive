cbba00725c747d322c9b4997a247f71aa626adc0
ankitchauhan123 <ankit.c7@gmail.com>
2021-10-27 17:38:23 +0530

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,5 +1,8 @@
 job('go-chat'){
     description ("Chat build and deployment job ")
+    wrappers {
+        golang('Go 1.17.2')
+    }
     scm{
         github('ankitchauhan123/chat','main')
     }
