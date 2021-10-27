d76782c77a467ed8e09cae7cfb693bea54b53da1
Ankit Chauhan <ankitchauhan@kuldeeps-Air.localdomain>
2021-10-27 13:02:17 +0530

    added jenkins dsl job

---
+++ jenkins.groovy
@@ -0,0 +1,16 @@
+job('go-chat'){
+    description ("Chat build and deployment job ",${new Date()})
+    scm{
+        git("https://github.com/ankitchauhan123/chat.git",main)
+    }
+    triggers{
+        scm('* * * * *')
+    }
+    steps{
+        shell('go build -o ./build/chat_build ./cmd')
+    }
+    publishers {
+            archiveArtifacts('build/*')
+    }
+
+}
